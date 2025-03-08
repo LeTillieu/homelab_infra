ee537d91ca07d4cd81be0f4d0ed207ffa3aca495
leo.tillieu <leo.tillieu@gmail.com>
2025-03-08 23:23:00 +0100

    Test variables in terraform

---
+++ terraform/deploy.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "gcs" {
+    bucket = var.gcp_bucket_name
+    prefix = "terraform/state"
+  }
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.71.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = "https://${var.proxmox_ip}:${var.proxmox_port}"
+  insecure = true
+  api_token = var.proxmox_api_token
+  ssh {
+    username = "terraform-prov"
+    private_key = var.terraform_private_key
+    node  {
+      name = "proxmox"
+      address = var.proxmox_ip
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "gcp_bucket_name" {
+  description = "Cloud Storage bucket name"
+  type        = string
+  sensitive   = false
+}
+
+variable "terraform_private_key" {
+  description = "Private key to access proxmox server"
+  type        = string
+  sensitive   = true
+}
+
+variable "proxmox_api_token" {
+  description = "Token for proxmox API"
+  type        = string
+  sensitive   = true
+}
+
+variable "proxmox_ip" {
+  description = "Proxmox ip address"
+  type        = string
+  sensitive   = true
+}
+
+variable "proxmox_port" {
+  description = "Proxmox port"
+  type        = string
+  sensitive   = true
+}
